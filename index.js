7d229becd5451313ca0a344085cb3b32e175e542
Kevin Huynh <kevin.huynh@orbeeauto.com>
2020-04-18 16:27:00 -0700

    fetch variants from URL

---
+++ index.js
@@ -0,0 +1,34 @@
+addEventListener('fetch', event => {
+
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+
+
+async function handleRequest(request) {
+
+  let v = await getVariants();
+
+  const randomVariant = v[Math.floor(Math.random() * v.length)];
+  return fetch(randomVariant);
+
+}
+
+
+async function getVariants(){
+  let url = "https://cfw-takehome.developers.workers.dev/api/variants"
+
+  let variants;
+
+  request = new Request(url)
+
+  const res = await fetch(url)
+  const data = await res.json()
+
+  variants = data.variants
+
+  return variants
+}
